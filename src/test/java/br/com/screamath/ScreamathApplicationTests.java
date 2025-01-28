391778d33dd4ec098c6eacc85fb098c71b26ee0f
Adriano Contesini <104787052+AContesini@users.noreply.github.com>
2025-01-28 20:40:07 -0300

    Primeiro Commit

---
+++ src/test/java/br/com/screamath/ScreamathApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.screamath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreamathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
